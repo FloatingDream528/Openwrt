b53feab82afcfc21a5b182e32a82222396ebc36d
FloatingDream528 <82099714+FloatingDream528@users.noreply.github.com>
2022-04-17 21:44:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,22 +13,17 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-#添加额外软件包
-git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-git clone https://github.com/fw876/helloworld.git package/helloworld
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns package/luci-app-smartdns
+git clone https://github.com/jerrykuku/luci-app-ttnode.git package/ttnode
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt  package
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+rm -rf feeds/packages/net/smartdns
 svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns package/smartdns
-#删除passwall重复组件
-rm -rf package/passwall/naiveproxy
-rm -rf package/passwall/shadowsocks-rust
-rm -rf package/passwall/shadowsocksr-libev
-rm -rf package/passwall/simple-obfs
-rm -rf package/passwall/tcping
-rm -rf package/passwall/trojan
-rm -rf package/passwall/v2ray-core
-rm -rf package/passwall/v2ray-geodata
-rm -rf package/passwall/v2ray-plugin
-rm -rf package/passwall/xray-core
-rm -rf package/passwall/xray-plugin
+svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-smartdns package/luci-app-smartdns
+svn co https://github.com/riverscn/openwrt-iptvhelper/trunk/iptvhelper package/iptvhelper
+svn co https://github.com/riverscn/openwrt-iptvhelper/tree/master/luci-app-iptvhelper package/luci-app-iptvhelper
+svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman package/luci-app-dockerman
+svn co https://github.com/gd0772/package/trunk/luci-app-usb-printer package/luci-app-usb-printer
